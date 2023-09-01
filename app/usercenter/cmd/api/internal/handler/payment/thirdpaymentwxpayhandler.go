8a84af8b6c7d8ad94e55b5e49f0b1c4acbe0cad1
Adulatian <jin.tian984@outlook.com>
2023-09-01 15:59:27 +0800

    weixin payment api request.

---
+++ app/usercenter/cmd/api/internal/handler/payment/thirdpaymentwxpayhandler.go
@@ -0,0 +1,28 @@
+package payment
+
+import (
+	"net/http"
+
+	"catroom/app/usercenter/cmd/api/internal/logic/payment"
+	"catroom/app/usercenter/cmd/api/internal/svc"
+	"catroom/app/usercenter/cmd/api/internal/types"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func ThirdPaymentwxPayHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.ThirdPaymentWxPayReq
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := payment.NewThirdPaymentwxPayLogic(r.Context(), svcCtx)
+		resp, err := l.ThirdPaymentwxPay(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
