d3e892ea766d25cd4f75cb459b508641119bec80
Adulatian <jin.tian984@outlook.com>
2023-09-19 15:14:27 +0800

    generate sql to model.

---
+++ app/usercenter/model/goodsModel.go
@@ -0,0 +1,24 @@
+package model
+
+import "github.com/zeromicro/go-zero/core/stores/sqlx"
+
+var _ GoodsModel = (*customGoodsModel)(nil)
+
+type (
+	// GoodsModel is an interface to be customized, add more methods here,
+	// and implement the added methods in customGoodsModel.
+	GoodsModel interface {
+		goodsModel
+	}
+
+	customGoodsModel struct {
+		*defaultGoodsModel
+	}
+)
+
+// NewGoodsModel returns a model for the database table.
+func NewGoodsModel(conn sqlx.SqlConn) GoodsModel {
+	return &customGoodsModel{
+		defaultGoodsModel: newGoodsModel(conn),
+	}
+}
