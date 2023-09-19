7fd167af439935029398f4834d1445c9332fccdf
Adulatian <jin.tian984@outlook.com>
2023-09-19 15:14:06 +0800

    generate sql to model.

--- app/usercenter/model/sql/product.sql
+++ app/usercenter/model/sql/product.sql
@@ -12,8 +12,10 @@ CREATE TABLE goods
     goods_sort        INT(11) UNSIGNED    NOT NULL DEFAULT '100' COMMENT '商品排序(数字越小越靠前)',
     goods_status      TINYINT(3) UNSIGNED NOT NULL DEFAULT '10' COMMENT '商品状态(10上架 20下架)',
     is_delete         TINYINT(3) UNSIGNED NOT NULL DEFAULT '0' COMMENT '是否删除',
-    create_time       INT(11) UNSIGNED    NOT NULL DEFAULT '0' COMMENT '创建时间',
-    update_time       INT(11) UNSIGNED    NOT NULL DEFAULT '0' COMMENT '更新时间'
+    `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `delete_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `del_state` tinyint NOT NULL DEFAULT '0'
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8 COMMENT ='商品记录表';
 
@@ -25,8 +27,10 @@ CREATE TABLE `goods_category`
     `parent_id`   int(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '上级分类id',
     `image_id`    int(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '分类图片id',
     `sort`        int(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '排序方式(数字越小越靠前)',
-    `create_time` int(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '创建时间',
-    `update_time` int(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '更新时间'
+    `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `delete_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `del_state` tinyint NOT NULL DEFAULT '0'
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8 COMMENT ='商品分类表';
 
@@ -43,7 +47,10 @@ CREATE TABLE goods_upload_file
     is_user     int(11) UNSIGNED    NOT NULL DEFAULT '0' COMMENT '是否为c端用户上传',
     is_recycle  tinyint(3) UNSIGNED NOT NULL DEFAULT '0' COMMENT '是否已回收',
     is_delete   tinyint(3) UNSIGNED NOT NULL DEFAULT '0' COMMENT '软删除',
-    create_time int(11) UNSIGNED    NOT NULL DEFAULT '0' COMMENT '创建时间'
+    `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `delete_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `del_state` tinyint NOT NULL DEFAULT '0'
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8 COMMENT ='图片记录表';
 
@@ -53,7 +60,10 @@ CREATE TABLE goods_image
     id          VARCHAR(255) PRIMARY KEY COMMENT '主键id',
     goods_id    int(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '商品id',
     image_id    int(11)          NOT NULL COMMENT '图片id(关联图片记录表)',
-    create_time int(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '创建时间'
+    `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `delete_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `del_state` tinyint NOT NULL DEFAULT '0'
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8 COMMENT ='商品图片记录表';
 
@@ -62,7 +72,10 @@ CREATE TABLE goods_spec
 (
     spec_id     VARCHAR(255) PRIMARY KEY COMMENT '规格组id',
     spec_name   varchar(255) NOT NULL DEFAULT '' COMMENT '规格组名称',
-    create_time int(11)      NOT NULL COMMENT '创建时间'
+    `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `delete_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `del_state` tinyint NOT NULL DEFAULT '0'
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8 COMMENT ='商品规格组记录表';
 
@@ -80,8 +93,10 @@ CREATE TABLE goods_sku
     stock_num    int(11) UNSIGNED        NOT NULL DEFAULT '0' COMMENT '当前库存数量',
     goods_sales  int(11) UNSIGNED        NOT NULL DEFAULT '0' COMMENT '商品销量',
     goods_weight double UNSIGNED         NOT NULL DEFAULT '0' COMMENT '商品重量(Kg)',
-    create_time  int(11) UNSIGNED        NOT NULL DEFAULT '0' COMMENT '创建时间',
-    update_time  int(11) UNSIGNED        NOT NULL DEFAULT '0' COMMENT '更新时间'
+    `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `delete_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `del_state` tinyint NOT NULL DEFAULT '0'
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8 COMMENT ='商品规格表';
 
@@ -91,6 +106,9 @@ CREATE TABLE spec_value
     spec_value_id VARCHAR(255) PRIMARY KEY COMMENT '规格值id',
     spec_value    varchar(255) NOT NULL COMMENT '规格值',
     spec_id       int(11)      NOT NULL COMMENT '规格组id',
-    create_time   int(11)      NOT NULL COMMENT '创建时间'
+    `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `delete_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `del_state` tinyint NOT NULL DEFAULT '0'
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8 COMMENT ='商品规格值记录表';
