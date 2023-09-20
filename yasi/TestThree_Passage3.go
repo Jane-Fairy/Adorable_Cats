c544743f392055f7b67ca92bf56e1ec1891a42eb
Adulatian <jin.tian984@outlook.com>
2023-09-20 11:53:30 +0800

    proto .

---
+++ yasi/TestThree_Passage3.go
@@ -0,0 +1,61 @@
+package yasi
+
+//Play is a Serious Business
+//Does Play Help Develop Bigger, Better Brains? Bryant Furlow Investigates.
+//A   Playing is a serious business. Children engrossed in a make-believe world, fox cubs play-fighting or kittens teasing a ball of string aren’t just having fun.
+//Play may look like a carefree and exuberant way to pass the time before the hard work of adulthood comes along, but there’s much more to it than that.
+//For a start, play can even cost animals their lives. Eighty percent of deaths among juvenile fur seals occur because playing pups fail to spot predators approaching.
+//It is also extremely expensive in terms of energy. Playful young animals use around two or three percent of their energy cavorting,
+//and in children that figure can be closer to fifteen percent.‘Even two or three percent is huge,’ says John Byers of Idaho University.
+//‘You just don’t find animals wasting energy like that,’ he adds. There must be a reason.
+//
+//B   But if play is not simply a developmental hiccup, as biologists once thought, why did it evolve?
+//The latest idea suggests that play has evolved to build big brains.
+//In other words, playing makes you intelligent. Playfulness, it seems, is common only among mammals, although a few of the larger-brained birds also indulge.
+//Animals at play often use unique signs-tail-wagging in dogs, for example—to indicate that activity superficially resembling adult behavior is not really in earnest.
+//A popular explanation of play has been that it helps juveniles develop the skills they will need to hunt, mate and socialize as adults.
+//Another has been that it allows young animals to get in shape for adult life by improving their respiratory endurance. Both these ideas have been questioned in recent years.
+//
+//C   Take the exercise theory. If play evolved to build muscle or as a kind of endurance training, then you would expect to see permanent benefits.
+//But Byers points out that the benefits of increased exercise disappear rapidly after training stops,
+//so any improvement in endurance resulting from juvenile play would be lost by adulthood. ‘If the function of play was to get into shape,’
+//says Byers, ‘the optimum time for playing would depend on when it was most advantageous for the young of a particular species to do so.
+//But it doesn’t work like that.’ Across species, play tends to peak about halfway through the suckling stage and then decline.
+//
+//D   Then there’s the skills-training hypothesis. At first glance, playing animals do appear to be practicing the complex manoeuvres they will need in adulthood.
+//But a closer inspection reveals this interpretation as too simplistic.
+//In one study, behavioral ecologist Tim Caro, from the University of California, looked at the predatory play of kittens and their predatory behavior when they reached adulthood.
+//He found that the way the cats played had no significant effect on their hunting prowess in later life.
+//
+//E   Earlier this year, Sergio Pellis of Lethbridge University, Canada, reported that there is a strong positive link between brain size and playfulness among mammals in general.
+//Comparing measurements for fifteen orders of mammal, he and his team found larger brains (for a given body size) are linked to greater playfulness.
+//The converse was also found to be true.
+//Robert Barton of Durham University believes that, because large brains are more sensitive to developmental stimuli than smaller brains,
+//they require more play to help mould them for adulthood. ‘I concluded it’s to do with learning, and with the importance of environmental data to the brain during development,’
+//he says.
+//
+//F   According to Byers, the timing of the playful stage in young animals provides an important clue to what’s going on.
+//If you plot the amount of time a juvenile devotes to play each day over the course of its development,
+//you discover a pattern typically associated with a ‘sensitive period’-a brief development window during which the brain can actually
+//be modified in ways that are not possible earlier or later in life. Think of the relative ease with which young children-but not infants or adults-absorb language.
+//Other researchers have found that play in cats, rats and mice is at its most intense just as this ‘window of opportunity’ reaches its peak.
+//
+//G   ‘People have not paid enough attention to the amount of the brain activated by play,’says Marc Bekoff from Colorado University.
+//Bekoff studied coyote pups at play and found that the kind of behavior involved was markedly more variable and unpredictable than that of adults.
+//Such behavior activates many different parts of the brain, he reasons.
+//Bekoff likens it to a behavioral kaleidoscope, with animals at play jumping rapidly between activities.
+//‘They use behavior from a lot of different context-- predation, aggression, reproduction,’ he says. ‘Their developing brain is getting all sorts of stimulation.’
+//
+//H   Not only is more of the brain involved in play than was suspected, but it also seems to activate higher cognitive processes.
+//‘There’s enormous cognitive, involvement in play,’ says Bekoof. He points out that play often involves complex assessments of playmates,
+//ideas of reciprocity and the use of specialized signals and rules.
+//He believes that play creates a brain that has greater behavioral flexibility and improved potential for learning later in life.
+//The idea is backed up by the work of Stephen Siviy of Gettysburg College.
+//Siviy studied how bouts of play affected the brain’s levels of a particular chemical associated with the stimulation and growth of nerve cells.
+//He was surprised by the extent of the activation. ‘Play just lights everything up,’ he says.
+//By allowing link-ups between brain areas that might not normally communicate with each other, play may enhance creativity.
+//
+//I   What might further experimentation suggest about the way children are raised in many societies today?
+//We already know that rat pups denied the chance to play grow smaller brain components and fail to develop the ability
+//to apply social rules when they interact with their peers.
+//With schooling beginning earlier and becoming increasingly exam-orientated, play is likely to get even less of a look-in. Who knows what the result of that will be?
